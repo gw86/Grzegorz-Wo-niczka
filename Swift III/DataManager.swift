87dd9d56c7129a386093ff05b889658fb2e5fa19
Grzegorz Woźniczka <grzegorz.wozniczka@miquido.com>
2016-12-19 09:31:51 +0100

    no error handling

--- Swift III/DataManager.swift
+++ Swift III/DataManager.swift
@@ -31,23 +31,10 @@ final class DataManager {
         let URL = baseURL
         
         URLSession.shared.dataTask(with: URL) { (data, response, error) in
-            self.didFetchLoginData(data: data, response: response, error: error, completion: completion)
-            }.resume()
+            self.processLoginData(data: data!, completion: completion)            }.resume()
     }
     
-    private func didFetchLoginData(data: Data?, response: URLResponse?, error: Error?, completion: LoginsDataCompletion) {
-        
-        if let data = data, let response = response as? HTTPURLResponse {
-            if response.statusCode == 200 {
-                processLoginData(data: data, completion: completion)
-                
-            } else {
-                completion(nil, .FailedRequest)
-            }
-        } else {
-            completion(nil, .Unknown)
-        }
-    }
+
     
     private func processLoginData(data: Data, completion: LoginsDataCompletion) {
         if let JSON = try? JSONSerialization.jsonObject(with: data, options: []) as AnyObject {
